c605aa27a736485569872dce603214241ad8a9de
Lina Kichen <hkichen@gmail.com>
2018-08-14 16:07:26 -0400

    cover page animation works

--- portfolio/src/components/Cover/Cover.js
+++ portfolio/src/components/Cover/Cover.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import './Cover.css';
 
 class Cover extends Component {
 	render() {
 		return (
-			<div className="welcome">
-			Hello World
+			<div className="container">
+				<div className="row">
+					<div className="banner-text">
+						<p id="line1">Full Stack</p>
+						<p id="line2">Web Developer</p>
+						<p id="line3">Lina Hoan Kichen </p>
+					</div>
+				</div>
 			</div>
-		)
+		)}
 	}
-}
-
-export default Cover;
+	
+	export default Cover;
